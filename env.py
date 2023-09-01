96434bedc46f4d006beada150f2529affa2dc3b1
emilalmander <emilalmander@hotmail.se>
2023-09-01 11:24:44 +0000

    deployment commit

--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://dkpxzbks:MqQtQ_U3cn6q1AUsTSMaTYdxjQO1QuOX@trumpet.db.elephantsql.com/dkpxzbks"
 os.environ["SECRET_KEY"]="hemligaNyckeln"
+os.environ["CLOUDINARY_URL"] = "cloudinary://858989525733224:9DkWKd5wwuFm9_oE05JpnmmUBeg@dsnyq5vps"
